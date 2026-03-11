575a54f8c6d8f23a39422af0a25021249cb89cf4
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-03-11 14:35:48 +0100

    ex 1 done

---
+++ src/es1/Main.java
@@ -0,0 +1,9 @@
+package es1;
+
+public class Main {
+    static void main(String[] args) {
+        Rettangolo rettangolo1 = new Rettangolo(10, 10);
+        Rettangolo rettangolo2 = new Rettangolo(10, 20);
+        Rettangolo.stampaRettangoli(rettangolo1, rettangolo2);
+    }
+}
---
+++ src/es1/Rettangolo.java
@@ -0,0 +1,68 @@
+package es1;
+
+public class Rettangolo {
+    private final double altezza;
+    private final double larghezza;
+
+    public Rettangolo(double altezza, double larghezza) {
+        this.altezza = altezza;
+        this.larghezza = larghezza;
+    }
+
+    public double calcPerimetro() {
+        return (this.getAltezza() + this.getLarghezza()) * 2;
+    }
+
+    public double calcArea() {
+        return this.getAltezza() * this.getLarghezza();
+    }
+
+    @Override
+    public String toString() {
+        return "Rettangolo{" +
+                "area=" + this.calcArea() +
+                ", perimetro=" + this.calcPerimetro() +
+                '}';
+    }
+
+    public static void stampaRettangolo(Rettangolo rettangolo) {
+        System.out.println(rettangolo);
+    }
+
+    public static void stampaRettangoli(Rettangolo rettangolo1, Rettangolo rettangolo2) {
+        Rettangolo[] rettangoli = {rettangolo1, rettangolo2};
+        System.out.println(rettangolo1);
+        System.out.println(rettangolo2);
+        System.out.println("------");
+        System.out.println("SOMMA AREE: " + calcAree(rettangoli));
+        System.out.println("SOMMA PERIMETRI: " + calcPerimetri(rettangoli));
+    }
+
+    public static double calcAree(Rettangolo[] rettangoli) {
+        double somma = 0;
+        for (Rettangolo rettangolo : rettangoli) {
+            somma += rettangolo.calcArea();
+        }
+        return somma;
+    }
+
+    public static double calcPerimetri(Rettangolo[] rettangoli) {
+        double somma = 0;
+        for (Rettangolo rettangolo : rettangoli) {
+            somma += rettangolo.calcPerimetro();
+        }
+        return somma;
+    }
+
+    //     GETTERS & SETTERS
+    public double getAltezza() {
+        return this.altezza;
+    }
+
+
+    public double getLarghezza() {
+        return this.larghezza;
+    }
+
+
+}
